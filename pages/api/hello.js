114c5fa9482af93e9173bf17ffd7f016c6e3cbdd
BuildTools <unconfigured@null.spigotmc.org>
2025-09-23 20:41:06 -0700

    Initial project setup with next.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const name = req.query.name || "input";
+  res.status(200).json({ greeting: `Hi ${name}` });
 }
+  
