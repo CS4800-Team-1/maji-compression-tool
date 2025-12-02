6a5e2c30d89f3c902bde2876ef56936a22b3b2ee
atarng-cyber <119980179+atarng-cyber@users.noreply.github.com>
2025-12-01 17:51:07 -0800

    Add Google Analytics script to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-S5GC629TYY"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXX', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
